e04961b7d59813c19d6094f2743e66b2db8c5d01
Nivea <nivea@respondeai.com.br>
2021-08-31 14:12:16 -0300

    preparando para o review

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-    <div className="header">
-        <div className="titulo">Driven Eats</div>
-        <div className="subtitulo">Sua comida em 6 minutos</div>
-    </div>
-    );
-}
